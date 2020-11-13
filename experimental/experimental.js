bd3daafe81fd1fa81cefb721a59fad313cd28a1b
wsdfhjxc <wsdfhjxc@op.pl>
2020-11-13 02:36:53 +0100

    Add delay function

--- experimental/experimental.js
+++ experimental/experimental.js
@@ -1,3 +1,13 @@
+function delay(milliseconds, callbackFunc) {
+    var timer = new QTimer();
+    timer.timeout.connect(function() {
+        timer.stop();
+        callbackFunc();
+    });
+    timer.start(milliseconds);
+    return timer;
+}
+
 function runCommand(command, parameters) {
     callDBus("org.kde.klauncher5", "/KLauncher", "org.kde.KLauncher",
              "exec_blind", command, parameters);
@@ -9,4 +19,6 @@ function showNotification(message, title, icon) {
     runCommand("kdialog", ["--icon", icon, "--title", title, "--passivepopup", message]);
 }
 
-showNotification("This is a message", "This is a title", "help-contextual");
+delay(3000, function() {
+    showNotification("This is a message", "This is a title", "help-contextual");
+});
