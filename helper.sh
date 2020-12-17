b0f582d83c9c4b8bf43eb0a5aa24cdeca51f72e1
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-17 20:50:36 +0100

    Update helper script

--- helper.sh
+++ helper.sh
@@ -1,29 +1,56 @@
-#!/bin/sh
+#!/bin/bash
+
+printUsage() {
+    echo "Usage: helper.sh name-of-the-script install|uninstall|upgrade|package"
+    echo "   or: helper.sh showInteractiveConsole"
+}
 
 case $1 in
-install)
-    kpackagetool5 -i .
+    install|uninstall|upgrade|package)
+        scriptName=$2
+
+        [[ -z "$scriptName" ]] && {
+            printUsage
+            exit 1
+        }
+
+        case $1 in
+            install)
+                kpackagetool5 -i "$scriptName"
+                ;;
+
+            uninstall)
+                kpackagetool5 -r "$scriptName"
+                ;;
+
+            upgrade)
+                kpackagetool5 -u "$scriptName"
+                ;;
+
+            package)
+                cd "$scriptName" 2>/dev/null || {
+                    echo "No such script '$scriptName'"
+                    exit 1
+                }
+
+                scriptVersion=$(grep -Po "Version=\K(.*)" metadata.desktop)
+                zip -r "$scriptName-$scriptVersion.kwinscript" contents metadata.desktop
+
+                cd ..
+                ;;
+            *)
+                printUsage
+                exit 1
+                ;;
+        esac
     ;;
 
-uninstall)
-    kpackagetool5 -r .
-    ;;
+    showInteractiveConsole)
+        qdbus org.kde.plasmashell /PlasmaShell showInteractiveKWinConsole
+        ;;
 
-upgrade)
-    kpackagetool5 -u .
-    ;;
-
-package)
-    name=$(basename "$(pwd)")
-    version=$(grep -Po "Version=\K(.*)" metadata.desktop)
-    zip -r "$name-$version.kwinscript" contents metadata.desktop
-    ;;
-
-show-console)
-    qdbus org.kde.plasmashell /PlasmaShell showInteractiveKWinConsole
-    ;;
-
-*)
-    echo "Usage: helper.sh install|uninstall|upgrade|package|show-console"
-    ;;
+    *)
+        printUsage
+        exit 1
+        ;;
 esac
